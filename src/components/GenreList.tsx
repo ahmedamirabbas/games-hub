f2c8a53e5c19306fecc582df5a66543f682f7add
Ahmed Amir <ahmad.abdulkhalig@gmail.com>
2023-10-01 17:16:26 +0200

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,34 @@
-import useGenres from '../hooks/useGenres';
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from '@chakra-ui/react';
+import useGenres, { Genre } from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenraList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
-      {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
