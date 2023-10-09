8994784eebc2f6cf2ac25f972ee0ad9ad665304d
Ahmed Amir <ahmad.abdulkhalig@gmail.com>
2023-10-09 17:08:34 +0200

    Extract a query object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from './../App';
 import useData from './useData';
 import { Genre } from './useGenres';
 
@@ -15,14 +16,16 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+const useGames = (GameQuery: GameQuery) =>
   useData<Game>(
     '/games',
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
-    [selectedGenre?.id, selectedPlatform?.id]
+    {
+      params: {
+        genres: GameQuery.genre?.id,
+        platforms: GameQuery.platform?.id,
+      },
+    },
+    [GameQuery]
   );
 
 export default useGames;
